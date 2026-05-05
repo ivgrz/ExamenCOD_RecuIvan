9eb7eac3e03adac73e90e0026ff7125334c70a1a
ivgrz <igutierrezcontreras@danielcastelao.org>
2026-05-05 13:43:22 +0200

    Cambio_rama_datos

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         // Imprime un mensaje
         System.out.println("Hola Examen!");
+        System.out.println("Realizando el examen...");
     }
 }
